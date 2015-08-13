67d895547242520fbec74d0f236621eb7375a141
scottsmeester <scott@smeester.com>
2015-08-12 19:09:22 -0600

    cleaning up

--- routes/hits.js
+++ routes/hits.js
@@ -1,12 +1,3 @@
-// var express = require('express');
-// var router = express.Router();
-
-// /* GET home page. */
-// router.get('/hits', function(req, res, next) {
-//   res.send('hits');
-// });
-
-// module.exports = router;
 var hits = 0;
 myHits = {
   count: function(req, res) {
